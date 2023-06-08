0c0d06e64b64f0c2d6bc717e7a7e7e07be2d4405
PilotSoul <theademari@gmail.com>
2023-06-08 11:32:54 +0800

    added apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'worker_list.apps.WorkerListConfig',
+    'gsm.apps.GsmConfig',
+    'kbs.apps.KbsConfig',
+    'signs.apps.SignsConfig',
 ]
 
 MIDDLEWARE = [
